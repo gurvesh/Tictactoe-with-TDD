b799629b5517525c9b3751a2dee4547fe3e8ac8d
gurvesh <guru01@gmail.com>
2015-06-14 11:59:32 +0100

    Imported hiccup

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ring/ring-core "1.3.2"]
-                 [ring/ring-jetty-adapter "1.3.2"]]
+                 [ring/ring-jetty-adapter "1.3.2"]
+                 [hiccup "1.0.5"]]
   :profiles {:dev {:dependencies [[speclj "3.2.0"]]}}
   :plugins [[speclj "3.2.0"]]
   :test-paths ["spec"]
