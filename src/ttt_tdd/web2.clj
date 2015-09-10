5d7a9312b8de428884069a952dfd7652b1642e69
gurvesh <guru01@Fox17com.local>
2015-09-10 07:06:43 +0100

    Enlive!

--- src/ttt_tdd/web2.clj
+++ src/ttt_tdd/web2.clj
@@ -5,43 +5,50 @@
    [ring.middleware.keyword-params :refer [wrap-keyword-params]]
    [ring.middleware.session :refer [wrap-session]]
    [ring.middleware.resource :refer [wrap-resource]]
-   [hiccup.core :refer [html]]
-   [ttt-tdd.ai :refer [next-board
+   [ttt-tdd.ai :refer [empty-board
+                       next-board
                        add-move-to-board
-                       score-win-or-draw]]
-   [net.cgrand.enlive-html :as h]))
+                       score-win-or-draw
+                       available-moves]]
+   [net.cgrand.enlive-html :as html]))
+
+(html/deftemplate start-html "ttt_tdd/start.html" [] [:html] identity)
 
 (def start-page
   {:status 200
-   :body (html
-          [:p "Click one of the two links to get started"]
-          [:a {:href "/computer"} "Start new game with Computer starting"])
+   :body (start-html)
    :session {:current-board empty-board}})
 
-(h/defsnippet button "ttt_tdd/button.html"
+(html/defsnippet button "ttt_tdd/button.html"
   [:button]
   [id]
-  (h/set-attr :value (str id)))
+  (html/set-attr :value (str id)))
 
-(h/defsnippet form "ttt_tdd/button.html"
+(html/defsnippet form "ttt_tdd/button.html"
   [:form]
   [id]
-  (h/content (button id)))
+  (html/content (button id)))
 
-(h/deftemplate index "ttt_tdd/ttt.html"
+(html/deftemplate play-html "ttt_tdd/ttt.html"
   [board]
   [:div] (fn [match]
-           (let [id (Integer. (first (h/attr-values match :id)))]
+           (let [id (Integer. (first (html/attr-values match :id)))]
              (cond
-               ((:x-moves board) id) ((h/set-attr :class "cross") match)
-               ((:o-moves board) id) ((h/set-attr :class "nought") match)
-               :else ((h/content (form id)) match)))))
+               ((:x-moves board) id) ((html/set-attr :class "cross") match)
+               ((:o-moves board) id) ((html/set-attr :class "nought") match)
+               :else ((html/content (form id)) match))))
+  [:#restart] (fn [match]
+                (if (score-win-or-draw board)
+                  (identity match)
+                  ((html/set-attr :visibility "hidden") match))))
 
-(defn computer-page [board]
-  (let [new-board (when-not (score-win-or-draw board)
-                    (next-board board))]
+(defn play-page [board computer-starting?]
+  (let [new-board (cond
+                    (score-win-or-draw board) board
+                    (and (not computer-starting?)(= board empty-board)) empty-board
+                    :else (next-board board))]
     {:status 200
-     :body (apply str (index (or new-board board)))
+     :body (apply str (play-html new-board))
      :session {:current-board new-board}}))
 
 (defn main-handler [{:keys [session
@@ -57,7 +64,8 @@
                     (add-move-to-board new-move current-board))]
     (case uri
       "/" start-page
-      "/computer" (computer-page new-board))))
+      "/computer" (play-page new-board true)
+      "/human" (play-page new-board false))))
 
 (def handler
   (-> main-handler
@@ -66,6 +74,3 @@
       wrap-keyword-params
       (wrap-resource "public/css")))
 
-(defonce server
-  (jetty/run-jetty #'handler {:port 5000
-                              :join? false}))
