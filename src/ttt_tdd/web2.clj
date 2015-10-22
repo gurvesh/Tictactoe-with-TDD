1045d630a524354c0b684b9524f7708c120a48cc
gurvesh <guru01@Fox17com.local>
2015-10-22 21:56:35 +0100

    Fixed losing message

--- src/ttt_tdd/web2.clj
+++ src/ttt_tdd/web2.clj
@@ -59,7 +59,7 @@
                :else ((html/content (form loc)) match))))
   
   [:#restart] (display-if (score-win-or-draw board))
-  [:#lose] (display-if (= 1 (score-win-or-draw board)))
+  [:#lose] (display-if (> 0 (score-win-or-draw board)))
   [:#draw] (display-if (= 0 (score-win-or-draw board)))
   [:p#losses] (html/content (str "Lost " (:losses score)))
   [:p#draws] (html/content (str "Draws " (:draws score))))
